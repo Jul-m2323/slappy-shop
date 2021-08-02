102a28cb9365ac92e29fb127db25c913c3bbde11
Julian Mora <unqju.mo@outlook.com.ar>
2021-08-02 20:18:50 -0300

    entrega de react navbar

---
+++ slappy-shop/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from './componentes/navBar';
+
+
+export default function App(){
+    return (<NavBar/>)
+}
---
+++ slappy-shop/src/componentes/navBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import '../css/estilo.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+export default function NavBar (){
+    return (
+    <header>
+        <nav className="navbar bg-yellow navbar-expand-xl">
+			<div className="fixed-top">
+			    <button className="navbar-toggler miToggler " type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+			      <span className="navbar-toggler-icon"></span>
+			    </button>
+			</div>
+            <div>
+                <h1 className="miFont Titulos">Slappy Shop</h1>
+            </div>
+			<div id="navbarToggleExternalContent" className="collapse navbar-collapse miHeader">
+				<ul className="nav">
+			  	    <li className="nav-item">
+			   		<a className="nav-link active miFont navFontColor" href="index.html">Inicio</a>
+			  	    </li>
+			  	    <li className="nav-item">
+			   		<a className="nav-link miFont navFontColor" href="shop.html">shop</a>
+			  	    </li>
+			  	    <li className="nav-item">
+			   		<a className="nav-link miFont navFontColor" href="noticias.html">Noticias</a>
+			  	    </li>
+			  	    <li className="nav-item">
+			   		<a className="nav-link miFont navFontColor" href="nosotros.html">Nosotros</a>
+			  	    </li>
+			  	    <li className="nav-item">
+			   		<a className="nav-link miFont navFontColor" href="encontranos.html">Encontranos</a>
+			  	</li>
+			    </ul>
+		    </div>
+	    </nav>
+    </header>
+    )
+}
